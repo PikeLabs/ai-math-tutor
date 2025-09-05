61ca0e69fef8ea7a8e04b9c66d70d9a1bdcf7add
AndjDavis <andi.idavis@gmail.com>
2025-09-05 12:22:53 -0600

    moved useAuth to hooks, logic seperation between Professor Auth and Student sessionId

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,27 +1,33 @@
-import React, {
+import {
 	createContext,
-	useContext,
 	useEffect,
 	useMemo,
 	useState,
+	useCallback,
 } from "react";
 import {
 	professorLogin,
 	professorLogout,
 	checkIsProfessor,
 } from "../services/api";
+import { useSession } from "../hooks/useSession";
 
-const AuthContext = createContext(null);
-export const useAuth = () => useContext(AuthContext);
+export const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
 	const [isProfessor, setIsProfessor] = useState(false);
 	const [loading, setLoading] = useState(true);
+	const { clearSession, sessionId } = useSession();
 
 	// bootstrap from /auth/me
 	useEffect(() => {
 		(async () => {
 			try {
+				if (sessionId) {
+					setIsProfessor(false);
+					return;
+				}
+
 				const data = await checkIsProfessor();
 				setIsProfessor(!!data.isProfessor);
 			} catch (e) {
@@ -30,27 +36,30 @@ export default function AuthProvider({ children }) {
 				setLoading(false);
 			}
 		})();
-	}, []);
+	}, [sessionId]);
 
-	const login = async (password) => {
-		setIsProfessor(false);
-
-		try {
-			await professorLogin(password);
-
-			const { isProfessor } = await checkIsProfessor();
-			if (!isProfessor) {
-				throw new Error("Invalid Password");
-			}
-
-			setIsProfessor(true);
-			return true;
-		} catch (error) {
+	const login = useCallback(
+		async (password) => {
 			setIsProfessor(false);
-			console.error("Login failed:", error);
-			throw error;
-		}
-	};
+
+			try {
+				await professorLogin(password);
+
+				const { isProfessor } = await checkIsProfessor();
+				if (!isProfessor) {
+					throw new Error("Invalid Password");
+				}
+
+				setIsProfessor(true);
+				if (sessionId) clearSession();
+				return true;
+			} catch (error) {
+				setIsProfessor(false);
+				throw error;
+			}
+		},
+		[clearSession, sessionId]
+	);
 
 	const logout = async () => {
 		await professorLogout();
@@ -59,7 +68,7 @@ export default function AuthProvider({ children }) {
 
 	const value = useMemo(
 		() => ({ isProfessor, loading, login, logout }),
-		[isProfessor, loading]
+		[isProfessor, loading, login]
 	);
 
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
